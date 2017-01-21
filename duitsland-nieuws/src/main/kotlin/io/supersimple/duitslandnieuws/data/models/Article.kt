208d7b0632eec8d15c8c9c3ac2c1ceb59d051577
Arjan Duijzer <arjan@nxtstep.nl>
2017-01-21 22:05:34 +0100

    Add Article model

---
+++ duitsland-nieuws/src/main/kotlin/io/supersimple/duitslandnieuws/data/models/Article.kt
@@ -0,0 +1,20 @@
+package io.supersimple.duitslandnieuws.data.models
+
+import java.util.*
+
+data class Article(
+        val id: String,
+        val date: Date,
+        val modified: Date,
+        val slug: String,
+        val link: String,
+        val title: RenderableText,
+        val content: RenderableText,
+        val excerpt: RenderableText,
+        val author: String
+)
+
+data class RenderableText(
+        val rendered: String,
+        val protected: Boolean
+)
