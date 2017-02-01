da103490da7e71174228c84cff1d4dd37b876989
Arjan Duijzer <arjan@nxtstep.nl>
2017-02-01 18:13:07 +0100

    ArticleCloud + Test

--- duitsland-nieuws/src/main/kotlin/io/supersimple/duitslandnieuws/data/repositories/article/ArticleCloud.kt
+++ duitsland-nieuws/src/main/kotlin/io/supersimple/duitslandnieuws/data/repositories/article/ArticleCloud.kt
@@ -1,10 +1,11 @@
 package io.supersimple.duitslandnieuws.data.repositories.article
 
-import io.reactivex.Maybe
+import io.reactivex.Single
+import io.supersimple.duitslandnieuws.data.api.ArticleEndpoint
 import io.supersimple.duitslandnieuws.data.models.Article
 
-class ArticleCloud {
-    fun list(): Maybe<List<Article>> {
-        return Maybe.empty()
+class ArticleCloud(private val articleService: ArticleEndpoint) {
+    fun list(): Single<List<Article>> {
+        return articleService.list()
     }
 }
