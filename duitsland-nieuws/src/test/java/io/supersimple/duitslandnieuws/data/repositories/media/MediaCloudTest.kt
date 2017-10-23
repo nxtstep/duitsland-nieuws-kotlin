2ce89bca1806eef61b419b9a40442dba67488766
Arjan Duijzer <arjan@nxtstep.nl>
2017-10-23 22:17:00 +0200

    Use Wiremock for mocking REST calls

--- duitsland-nieuws/src/test/java/io/supersimple/duitslandnieuws/data/repositories/media/MediaCloudTest.kt
+++ duitsland-nieuws/src/test/java/io/supersimple/duitslandnieuws/data/repositories/media/MediaCloudTest.kt
@@ -1,48 +1,40 @@
 package io.supersimple.duitslandnieuws.data.repositories.media
 
-import android.app.Application
+import com.github.tomakehurst.wiremock.client.WireMock.aResponse
+import com.github.tomakehurst.wiremock.client.WireMock.get
+import com.github.tomakehurst.wiremock.client.WireMock.stubFor
+import com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration
+import com.github.tomakehurst.wiremock.junit.WireMockRule
 import com.google.gson.FieldNamingPolicy
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
-import io.supersimple.duitslandnieuws.BuildConfig
 import io.supersimple.duitslandnieuws.data.api.MediaEndpoint
-import io.supersimple.duitslandnieuws.data.rest.AbsUnitTestRestApiTestCase
 import okhttp3.OkHttpClient
-import okhttp3.mockwebserver.Dispatcher
-import okhttp3.mockwebserver.MockResponse
-import okhttp3.mockwebserver.RecordedRequest
-import okio.Buffer
-import org.junit.After
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
-import org.robolectric.RobolectricTestRunner
-import org.robolectric.RuntimeEnvironment
-import org.robolectric.annotation.Config
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import java.io.IOException
 
-@RunWith(RobolectricTestRunner::class)
-@Config(constants = BuildConfig::class, sdk = intArrayOf(23))
-class MediaCloudTest : AbsUnitTestRestApiTestCase() {
+class MediaCloudTest {
 
     lateinit var networkService: MediaEndpoint
-
-    override val dispatcher: Dispatcher
-        get() = dispatch
-    override val app: Application
-        get() = RuntimeEnvironment.application
 
     private val gsonConverter: Gson = GsonBuilder()
             .setDateFormat("yyyy-MM-dd'T'HH:mm:ss")
             .setFieldNamingPolicy(FieldNamingPolicy.IDENTITY)
             .create()
 
+    @get:Rule
+    val wireMock = WireMockRule(WireMockConfiguration.wireMockConfig().dynamicPort())
+
+    private lateinit var baseUrl: String
+
     @Before
-    override fun setup() {
-        super.setup()
+    fun setup() {
+        baseUrl = "http://localhost:${wireMock.port()}"
 
         networkService = Retrofit.Builder()
                 .baseUrl(baseUrl)
@@ -53,13 +45,18 @@ class MediaCloudTest : AbsUnitTestRestApiTestCase() {
                 .create(MediaEndpoint::class.java)
     }
 
-    @After
-    override fun tearDown() {
-        super.tearDown()
-    }
-
     @Test
     fun testGet() {
+        stubFor(
+                get(urlEqualTo("/media/test-id"))
+                        .willReturn(
+                                aResponse()
+                                        .withHeader("Content-Type", "application/json; charset=UTF-8")
+                                        .withStatus(200)
+                                        .withBodyFile("media_49979.json")
+                        )
+        )
+
         val cloud = MediaCloud(networkService)
         cloud.get("test-id")
                 .test()
@@ -77,36 +74,5 @@ class MediaCloudTest : AbsUnitTestRestApiTestCase() {
                 .assertValue { it.media_details.sizes["thumbnail"]!!.mime_type == "image/png" }
                 .assertValue { it.media_details.sizes["thumbnail"]!!.source_url == "http://duitslandnieuws.nl/wp-content/uploads/2017/01/Schermafdruk-2017-01-07-16.04.53-150x150.png" }
 
-    }
-
-    private val dispatch = object : Dispatcher() {
-        override fun dispatch(request: RecordedRequest): MockResponse {
-            val response = MockResponse()
-            var fileName: String? = null
-            val requestPath = request.path
-            println("Dispatcher requestPath: $requestPath\n")
-            if (requestPath == "/media/test-id") {
-                response.setHeader("Content-Type", "application/json")
-                response.setResponseCode(200)
-                fileName = "media_49979.json"
-            } else {
-                // Unhandled request
-                response.setResponseCode(500)
-            }
-
-            fileName?.let {
-                try {
-                    val inputStream = getInputStreamForFile(it)
-                    val buffer = Buffer()
-                    buffer.readFrom(inputStream)
-                    response.body = buffer
-                    inputStream.close()
-                } catch (e: IOException) {
-                    println("Exception while preparing Response body: " + e.message)
-                }
-            }
-
-            return response
-        }
     }
 }
